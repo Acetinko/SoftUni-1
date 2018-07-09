4d0c119b67f6703ec8fc0145698c2941d387b01d
Peter Georgiev <peroxmen@hotmail.com>
2018-07-09 22:31:05 +0300

    :)

--- JavaScript_for_Front-End/06.DOM_EVENTS-EXERCISE/01. Subtraction/subtract.js
+++
@@ -1,5 +0,0 @@
-function subtract() {
-    let num1Str = document.getElementById("firstNumber").value;
-    let num2Str = document.getElementById("secondNumber").value;
-    document.getElementById("result").textContent = Number(num1Str) - Number(num2Str);
-}
