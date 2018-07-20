89dd18dc54d47dd4576b5495e347281dd2b90617
Peter-Georgiev <peter.g.georgiev@gmail.com>
2018-07-20 19:16:52 +0300

    :)

--- JavaScript_for_Front-End/10.JQUERY_PLUGINS-EXERCISE/01. Slick-Slider/app.js
+++ JavaScript_for_Front-End/10.JQUERY_PLUGINS-EXERCISE/01. Slick-Slider/app.js
@@ -1,7 +1,26 @@
-
 $(".cats").slick({
     dots: true,
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3
 });
+
+(() => {
+    let div = $('.cats div.slick-cloned');
+
+    for (let i = 0; i < div.length; i++) {
+        let tmpSrc = (div[i].children[0]).getAttribute('src');
+        if (tmpSrc == null) {
+            continue;
+        }
+        let tmpNum = +(tmpSrc.split('img/')[1]).split('.')[0];
+
+        if (tmpNum % 3 === 1) {
+            continue;
+        }
+
+        $(div[i]).fadeTo("slow", 0.5, function () {
+            // Animation complete.
+        });
+    }
+})();
