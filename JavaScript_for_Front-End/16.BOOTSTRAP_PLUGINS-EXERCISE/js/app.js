902b34e0f09c6c75ba101ad7c292e6bc9c4739cc
Peter-Georgiev <peter.g.georgiev@gmail.com>
2018-08-03 14:59:04 +0300

    :)

--- JavaScript_for_Front-End/16.BOOTSTRAP_PLUGINS-EXERCISE/js/app.js
+++ JavaScript_for_Front-End/16.BOOTSTRAP_PLUGINS-EXERCISE/js/app.js
@@ -1,3 +1,30 @@
+$('.btn.btn-facebook').on('click', function (ev) {
+    socialButton('http://facebook.com');
+});
 
-//$('.container').css('background','url("../img/hands-with-pencil-and-ruler.jpg") no-repeat center');
-$('.card').css('border-width','2px');
+$('.btn.btn-google').on('click', function (ev) {
+    socialButton('http://google.com');
+});
+
+function socialButton(url) {
+    swal({
+            title: "Leave this site?",
+            text: `If you click 'OK', you will be redirected to ${url}`,
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonClass: "btn-primary",
+            confirmButtonText: "OK",
+            closeOnConfirm: true
+        },
+        function(){
+            swal();
+            if (url === 'http://google.com') {
+
+                //url.log
+                href=`https://accounts.google.com/signup`;
+            } else if (url === 'http://facebook.com') {
+                href=`${url}/signup`;
+            }
+            window.open(`${href}`, 'mywin', 'left=50,top=80,width=600,height=600');
+        });
+}
